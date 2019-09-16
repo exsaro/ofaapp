9912139ae6092b555d888afa6f7c94598e087d8c
hari42726 <hari@sis.in>
2019-09-16 14:51:29 +0530

    update

--- src/app/services/shoppingapi.service.ts
+++ src/app/services/shoppingapi.service.ts
@@ -12,7 +12,8 @@ export class ShoppingapiService {
     url: 'https://www.softwebsystems.com/ofa/', // Your store URL
     consumerKey: 'ck_6ae751f70b993795144aa67a1e1b6dae6cf89b6a', // Your consumer key
     consumerSecret: 'cs_b8f40500f0a6056af076aaf06962b8a5af6e8cc4', // Your consumer secret
-    version: 'wc/v3' // WooCommerce WP REST API version
+    version: 'wc/v3', // WooCommerce WP REST API version
+    queryStringAuth: true
   });
 
    getAllProducts() {
